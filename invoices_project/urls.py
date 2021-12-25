f08a14f0a84525d8e4cc783a89d6c4bbd7e26f88
Abdul_rehman <76230556+Abdulrehman-PIAIC80387@users.noreply.github.com>
2021-12-25 18:00:27 +0500

    2files_search_set

---
+++ invoices_project/urls.py
@@ -0,0 +1,54 @@
+"""invoices_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from invoice_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home', views.home, name='home'),
+    path('add_invoice/', views.add_invoice, name='add_invoice'),
+    path('list_invoice/', views.list_invoice, name='list_invoice'),
+    path('list_invoice_search/', views.list_invoice_search, name='list_invoice_search'),
+    path('add_service/', views.add_service, name='add_service'),
+    path('list_services/', views.list_services, name='list_services'),
+    #path('list_invoice_print/', views.list_invoice_print, name='list_invoice_print'),
+    path('update_Service/<str:pk>/', views.update_Service, name="update_Service"),
+    path('delete_service/<str:pk>/', views.delete_service, name="delete_service"),
+    path('update_invoice/<str:pk>/', views.update_invoice, name="update_invoice"),
+    path('delete_invoice/<str:pk>/', views.delete_invoice, name="delete_invoice"),
+    path('add_expense/', views.add_expense, name='add_expense'),
+    path('list_expense/', views.list_expense, name='list_expense'),
+    path('update_expense/<str:pk>/', views.update_Expense, name="update_expense"),
+    path('delete_expense/<str:pk>/', views.delete_Expense, name="delete_expense"),
+    path('register/', views.registerPage, name="register"),
+	path('', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+    path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view"),
+    path('ViewPDF_print/<str:pk>/', views.ViewPDF_print.as_view(), name="ViewPDF_print"),
+    #path('ViewPDF_selected/', views.ViewPDF_selected, name="ViewPDF_selected"),
+    path('list_expense_search/', views.list_expense_search, name='list_expense_search'),
+    path('ViewPDF_print_expense/', views.ViewPDF_print_expense.as_view(), name="ViewPDF_print_expense"),
+    path('ViewPDF_print_expense_aleda/<str:pk>/', views.ViewPDF_print_expense_aleda, name="ViewPDF_print_expense_aleda"),
+    path('list_Travel_search/', views.list_Travel_search, name='list_Travel_search'),
+    path('ViewPDF_print_travel/', views.ViewPDF_print_travel.as_view(), name="ViewPDF_print_travel"),
+    path('ViewPDF_print_Travel_aleda/<str:pk>/', views.ViewPDF_print_Travel_aleda, name="ViewPDF_print_Travel_aleda"),
+    path('list_invoice_ledger/', views.list_invoice_ledger, name='list_invoice_ledger'),
+    path('update_invoice_ledger/<str:pk>/', views.update_invoice_ledger, name="update_invoice_ledger"),
+    path('ViewPDF_print_ledger/', views.ViewPDF_print_ledger.as_view(), name="ViewPDF_print_ledger"),
+    path('ViewPDF_print_leger_separte/<str:pk>/', views.ViewPDF_print_leger_separte, name="ViewPDF_print_leger_separte"),
+
+]
